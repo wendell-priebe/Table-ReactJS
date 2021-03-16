68918b6c5fa32f3397b17f1faefc59fb461b8cc3
Wendell Priebe <wendel_oliveira94@hotmail.com>
2021-03-16 15:15:52 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import Table from './Table';
+
+class App extends React.Component{
+    render(){
+      const name = 'Wendell'
+      return(
+        <div className="container">
+          <Table />
+        </div>
+      )
+    }
+}
+
+export default App;
+
+
+
+Adereços é o proximo a fazer !!!
---
+++ src/Table.js
@@ -0,0 +1,81 @@
+import React, {Component} from 'react';
+import { render } from 'react-dom';
+
+// class Table extends Component{
+//     render(){
+//         return(
+//             <table>
+//             <thead>
+//               <tr>
+//                 <th>Name</th>
+//                 <th>Job</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               <tr>
+//                 <td>Charlie</td>
+//                 <td>Janitor</td>
+//               </tr>
+//               <tr>
+//                 <td>Mac</td>
+//                 <td>Bouncer</td>
+//               </tr>
+//               <tr>
+//                 <td>Dee</td>
+//                 <td>Aspiring actress</td>
+//               </tr>
+//               <tr>
+//                 <td>Dennis</td>
+//                 <td>Bartender</td>
+//               </tr>
+//             </tbody>
+//           </table>
+//         )
+//     }
+// };
+
+const TableHeader = () => {
+  return(
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Job</th>
+      </tr>
+    </thead>
+  )
+};
+const TableBody = () => {
+  return(
+    <tbody>
+    <tr>
+      <td>Charlie</td>
+      <td>Janitor</td>
+    </tr>
+    <tr>
+      <td>Mac</td>
+      <td>Bouncer</td>
+    </tr>
+    <tr>
+      <td>Dee</td>
+      <td>Aspiring actress</td>
+    </tr>
+    <tr>
+      <td>Dennis</td>
+      <td>Bartender</td>
+    </tr>
+  </tbody>
+  )
+};
+
+class Table extends Component{
+  render(){
+    return(
+      <table>
+        <TableHeader />
+        <TableBody />
+      </table>
+    )
+  }
+}
+
+export default Table;
